50c7b638acd27c025d4f298482b9a737dc1ccabf
shaiannesunagawa <sys74530@creighton.edu>
2021-03-31 15:32:10 -0500

    not yet working

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -5,12 +5,15 @@ favExercises.onshow=function(){
     for (i = 0; i < exercises.length; i++) 
     selExercises.addItem(exercises[i])
     
+    /*
     for (i = 0; i < coreExercises.length; i++) 
     selExercises.addItem(coreExercises[i])
+    */
 }
 
 btnSelection.onclick=function(){
-    if (selExercises.text == coreExercises)
+    results = coreExercises
+    if (selExercises.text == results)
         lblAnswer.value = 'You chose situps and planks - those are the two core exercises.' 
     else
         lblAnswer.value = 'You did not pick the two core exercises.'
